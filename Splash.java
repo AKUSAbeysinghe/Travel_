df56d864fa80bdc69aca13ce3a107025460b3220
AKUSAbeysinghe <154555496+AKUSAbeysinghe@users.noreply.github.com>
2025-02-09 23:09:23 +0530

    Add files via upload

---
+++ Splash.java
@@ -0,0 +1,22 @@
+
+package travel;
+
+import javax.swing.*;
+
+
+public class Splash extends JFrame {
+
+    Splash(){
+    
+       setSize(1200, 600);
+       setVisible(true);
+    
+    }
+
+public static void main(String[] args){
+
+    new Splash();
+
+}
+    
+}
