8057b4fcd80191e7769ed373534a6f61ce0f3e5b
AKUSAbeysinghe <154555496+AKUSAbeysinghe@users.noreply.github.com>
2025-02-20 23:35:12 +0530

    Update Conn.java

--- Conn.java
+++ Conn.java
@@ -1,1 +1,25 @@
 
+package travel;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    
+  Conn(){
+  
+      try {
+          Class.forName("com.mysql.cj.jdbc.Driver");
+          c=DriverManager.getConnection("jdbc:mysql:///travel_management", "root", "Abc@1234");
+          s = c.createStatement();
+          
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+  
+  }  
+    
+}
+
